7b4c06d496023948748e2fd3c6d30625f60c6738
KrisTnv <krisso.k@abv.bg>
2021-04-22 11:39:37 +0300

    Added some functionality

---
+++ story.js
@@ -0,0 +1,9 @@
+function GoodEnding() {
+    alert('Congratulations, you saved the city and banished the evil from these lands. Our hero! (:');
+}
+function BadEnding() {
+    alert('The people never suspected you would betray your own kin, but one thing is for sure, they will have their revenge!');
+}
+function Restart() {
+    alert('Restarting the game!');
+}
